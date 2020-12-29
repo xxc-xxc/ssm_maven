e346a912e7f9748b6465dfc51c415553b36948f4
xxc-xxc <55835158+xxc-xxc@users.noreply.github.com>
2020-12-29 11:34:54 +0800

    创建业务层接口&实现类

---
+++ src/main/java/com/ssm/service/UserService.java
@@ -0,0 +1,14 @@
+package com.ssm.service;
+
+import com.ssm.domain.User;
+
+/**
+ * @Author xxc
+ * @Date 2020/12/29 11:14
+ * @Description TODO
+ */
+public interface UserService {
+
+    User findById(int userId);
+
+}
---
+++ src/main/java/com/ssm/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.ssm.service.impl;
+
+import com.ssm.dao.UserDao;
+import com.ssm.domain.User;
+import com.ssm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @Author xxc
+ * @Date 2020/12/29 11:15
+ * @Description TODO
+ */
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+
+//    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public User findById(int userId) {
+        return userDao.findById(userId);
+    }
+}
